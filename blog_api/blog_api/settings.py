a07148cf38bd2dd42e36b07f2054cdd69e62e4e3
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-25 19:50:33 +0100

    modified a lot

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -98,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
