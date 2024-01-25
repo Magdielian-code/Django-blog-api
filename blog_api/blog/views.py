5a94ef8a7e864b039d8054632d91c0429a0a8717
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-25 23:07:00 +0100

    modified a lot

--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'blog/home.html')
 
-def home(request):
-    return render(request, 'blog/home.html')
+def about(request):
+    return render(request, 'blog/about.html')
 
