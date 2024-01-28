bd60310a0e9d2c6d2e52bbc7d1505735802acd7a
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-28 01:46:24 +0100

    passed blog details to html file

--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Dave Smith',
+        'title': 'Blog post',
+        'content': 'Demo post',
+        'date_posted': 'January 27, 2024'
+    },
+    {
+        'author': 'Gregor Mendel',
+        'title': 'Blog post 2',
+        'content': 'Demo post 2',
+        'date_posted': 'January 30, 2024'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'post': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
