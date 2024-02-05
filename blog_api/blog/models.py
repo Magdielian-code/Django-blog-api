193815ab3b0d493030e5cd8b0ceb13279398d719
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-02-05 12:35:10 +0100

    updated Nome_write; added migrations

--- blog_api/blog/models.py
+++ blog_api/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# This model is for every post on Nome_write
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=100)
@@ -9,5 +10,6 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
+    # To make the post objects more descriptive when printed out
     def __str__(self):
         return self.title
--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Dave Smith',
-        'title': 'Blog post 1',
-        'content': 'Demo post',
-        'date_posted': 'January 27, 2024'
-    },
-    {
-        'author': 'Gregor Mendel',
-        'title': 'Blog post 2',
-        'content': 'Demo post 2',
-        'date_posted': 'January 30, 2024' 
-    }
-]
 
 def home(request):
     context = {
-        'post': posts
+        'post': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
