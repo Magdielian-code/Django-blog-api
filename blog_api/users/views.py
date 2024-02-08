7755a055e9429c79ccc7001061b5fa9f38bf0ecb
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-02-08 11:11:36 +0100

    fixed user app issue and views.py file

--- blog_api/users/views.py
+++ blog_api/users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'forms': form})
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account successfully created for {username}')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
